83bad7d27aa78bbada840eb05490a9d3cfb0e485
ashokkumar-autovyn <mrashok0205@gmail.com>
2025-02-18 18:00:34 +0530

    jkjkj

--- Fronted/src/store/faqsStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useFAQsStore = create((set) => ({
-    faqs: [],
-    addFAQ: (faq) => set((state) => ({ faqs: [...state.faqs, faq] })),
-    clearFAQs: () => set({ faqs: [] }),
-}));
-
-export default useFAQsStore;
